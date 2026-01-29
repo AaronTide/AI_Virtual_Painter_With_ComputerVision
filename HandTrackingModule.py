3335211f2feae791fbdbb775adc3ae55c2437ef4
Arifa Rahman Sikder <165793165+AaronTide@users.noreply.github.com>
2026-01-29 17:17:04 +0600

    Update HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -3,6 +3,8 @@ import mediapipe as mp
 
 import time
 
+
+
 class handDetector():
     def __init__(self, mode= False, maxHands= 2,  model_complexity = 1, detectionCon=0.5,trackCon=0.5):
         self.mode = mode
@@ -13,6 +15,7 @@ class handDetector():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.model_complexity, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 16, 20]
 
     def findHands(self, img, draw = True):
 
@@ -28,7 +31,7 @@ class handDetector():
         return img
 
     def findPosition (self, img, handNo=0, draw= True):
-        lmList = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
@@ -36,11 +39,30 @@ class handDetector():
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 #print(id, cx, cy)
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                 #if id == 4:
-                    cv2.circle(img, (cx, cy), 15,(255, 0, 255), cv2.FILLED)
-        return lmList
+                    cv2.circle(img, (cx, cy), 7,(255, 0, 0), cv2.FILLED)
+        return self.lmList
+
+    def fingersUp(self):
+        fingers = []
+        #thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0]-1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        #4 fingers
+        for id in range(1,5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
+
+
 
 def main():
     pTime = 0
